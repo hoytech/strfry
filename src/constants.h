4cff73b01bb54af71a2b58aac335969ad5d4fd7b
Doug Hoyte <doug@hcsw.org>
2023-06-08 16:23:12 -0400

    increase DB version to 2

--- src/constants.h
+++ src/constants.h
@@ -1,5 +1,5 @@
 #pragma once
 
-const uint64_t CURR_DB_VERSION = 1;
+const uint64_t CURR_DB_VERSION = 2;
 const size_t MAX_SUBID_SIZE = 71; // Statically allocated size in SubId
 const size_t MAX_INDEXED_TAG_VAL_SIZE = 255;
