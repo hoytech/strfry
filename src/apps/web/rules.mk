b5a0d560502e6a79dedaa9ffd42d0c9f3f3b862c
Doug Hoyte <doug@hcsw.org>
2023-09-08 08:02:22 -0400

    wip

--- src/apps/web/rules.mk
+++ src/apps/web/rules.mk
@@ -11,16 +11,16 @@ LDLIBS += -lre2
 build/web-static/oddbean.css: src/apps/web/static/reset.css src/apps/web/static/oddbean.css
 	mkdir -p build/web-static/
 	cat $^ | sassc -s -t compressed > $@
-	gzip -k9f $@
 
 build/web-static/oddbean.js: src/apps/web/static/base.ts src/apps/web/static/turbo.js src/apps/web/static/oddbean.js src/apps/web/static/alpine.js
 	mkdir -p build/web-static/
 	cat $^ | esbuild --loader=ts --minify > $@
-	gzip -k9f $@
 
 build/web-static/oddbean.svg: src/apps/web/static/oddbean.svg
 	cp $^ $@
-	gzip -k9f $@
 
-build/WebStaticFiles.h: build/web-static/oddbean.css build/web-static/oddbean.js build/web-static/oddbean.svg
+build/web-static/favicon.ico: src/apps/web/static/favicon.ico
+	cp $^ $@
+
+build/WebStaticFiles.h: build/web-static/oddbean.css build/web-static/oddbean.js build/web-static/oddbean.svg build/web-static/favicon.ico
 	perl golpe/external/hoytech-cpp/dirToCppHeader.pl build/web-static/ oddbeanStatic > $@
