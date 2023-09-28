f3580287f5fe663314c7e4db47a5409051fe0914
Doug Hoyte <doug@hcsw.org>
2023-09-28 16:47:37 -0400

    updates

--- src/apps/web/install-homepage-community.sh
+++ src/apps/web/install-homepage-community.sh
@@ -1,1 +1,2 @@
+## Following secret key is just a junk testing key
 nostril --envelope --sec c1eee22f68dc218d98263cfecb350db6fc6b3e836b47423b66c62af7ae3e32bb --content "$(perl -MYAML -MJSON::XS -E 'print encode_json(YAML::LoadFile(q{homepage-community.yaml}));')" --kind 33700 --tag d homepage | websocat ws://127.0.0.1:7777
