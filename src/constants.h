052c56e62f1873e08497077c126653d764809deb
Doug Hoyte <doug@hcsw.org>
2024-09-05 15:46:35 -0400

    bump DB version

--- src/constants.h
+++ src/constants.h
@@ -1,5 +1,5 @@
 #pragma once
 
-const uint64_t CURR_DB_VERSION = 2;
+const uint64_t CURR_DB_VERSION = 3;
 const size_t MAX_SUBID_SIZE = 71; // Statically allocated size in SubId
 const size_t MAX_INDEXED_TAG_VAL_SIZE = 255;
