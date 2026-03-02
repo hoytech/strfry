aeba0d8fea3107086838b16bba8d7c1ed389ae59
Doug Hoyte <doug@hcsw.org>
2026-03-02 16:37:54 -0500

    Support deletion of PREs, using a-tags

---
+++ src/EventUtils.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <string_view>
+#include <string>
+#include <tuple>
+#include <stdexcept>
+
+
+inline bool isReplaceableKind(uint64_t kind) {
+    return (
+        kind == 0 ||
+        kind == 3 ||
+        kind == 41 ||
+        (kind >= 10'000 && kind < 20'000)
+    );
+}
+
+inline bool isParamReplaceableKind(uint64_t kind) {
+    return (
+        (kind >= 30'000 && kind < 40'000)
+    );
+}
+
+inline bool isEphemeralKind(uint64_t kind) {
+    return (
+        (kind >= 20'000 && kind < 30'000)
+    );
+}
+
+
+inline std::tuple<uint64_t, std::string, std::string> parseATag(std::string_view input) {
+    size_t firstColon = input.find(':');
+    if (firstColon == std::string::npos) throw std::invalid_argument("parse error");
+
+    std::string_view kindStr = input.substr(0, firstColon);
+    size_t pos;
+    uint64_t kind = std::stoull(std::string(kindStr), &pos);
+    if (pos != kindStr.size()) throw std::invalid_argument("parse error");
+
+    size_t secondColon = input.find(':', firstColon + 1);
+    if (secondColon == std::string::npos) throw std::invalid_argument("parse error");
+
+    std::string_view pubkeyStr = input.substr(firstColon + 1, secondColon - firstColon - 1);
+    if (pubkeyStr.size() != 64) throw std::invalid_argument("parse error");
+
+    std::string dTag = std::string(input.substr(secondColon + 1));
+
+    return {kind, hoytech::from_hex(pubkeyStr), std::move(dTag)};
+}
--- src/events.h
+++ src/events.h
@@ -8,31 +8,7 @@
 #include "PackedEvent.h"
 #include "NegentropyFilterCache.h"
 #include "Decompressor.h"
-
-
-
-
-inline bool isReplaceableKind(uint64_t kind) {
-    return (
-        kind == 0 ||
-        kind == 3 ||
-        kind == 41 ||
-        (kind >= 10'000 && kind < 20'000)
-    );
-}
-
-inline bool isParamReplaceableKind(uint64_t kind) {
-    return (
-        (kind >= 30'000 && kind < 40'000)
-    );
-}
-
-inline bool isEphemeralKind(uint64_t kind) {
-    return (
-        (kind >= 20'000 && kind < 30'000)
-    );
-}
-
+#include "EventUtils.h"
 
 
 
