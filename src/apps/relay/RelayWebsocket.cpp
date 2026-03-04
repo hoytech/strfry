61f259c7808b26ab07ba5c9573cee972c4a5dfd4
Doug Hoyte <doug@hcsw.org>
2026-03-04 11:08:27 -0500

    Remove NIP 22 from support NIPs list. This NIP was folded into NIP-01, and is now something else.

--- src/apps/relay/RelayWebsocket.cpp
+++ src/apps/relay/RelayWebsocket.cpp
@@ -53,7 +53,7 @@ void RelayServer::runWebsocket(ThreadPool<MsgWebsocket>::Thread &thr) {
 
 
     auto supportedNips = []{
-        tao::json::value output = tao::json::value::array({ 1, 2, 4, 9, 11, 22, 28, 40, 70 });
+        tao::json::value output = tao::json::value::array({ 1, 2, 4, 9, 11, 28, 40, 70 });
 
         if (cfg().relay__maxFilterLimitCount > 0) output.push_back(45);
         if (cfg().relay__negentropy__enabled) output.push_back(77);
