e34e191a2146d1f9d6192e179f767541b6134626
Doug Hoyte <doug@hcsw.org>
2026-03-12 00:24:27 -0400

    upgrade bech32.cpp

--- src/bech32.h
+++ src/bech32.h
@@ -1,48 +1,48 @@
-/* Copyright (c) 2017, 2021 Pieter Wuille
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+// Copyright (c) 2017, 2021 Pieter Wuille
+// Copyright (c) 2021-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BECH32_H_
-#define BECH32_H_ 1
+// Bech32 and Bech32m are string encoding formats used in newer
+// address types. The outputs consist of a human-readable part
+// (alphanumeric), a separator character (1), and a base32 data
+// section, the last 6 characters of which are a checksum. The
+// module is namespaced under bech32 for historical reasons.
+//
+// For more information, see BIP 173 and BIP 350.
 
+#ifndef BITCOIN_BECH32_H
+#define BITCOIN_BECH32_H
+
+#include <cstdint>
 #include <string>
-#include <tuple>
 #include <vector>
-
-#include <stdint.h>
 
 namespace bech32
 {
 
-enum class Encoding {
-    INVALID,
+static constexpr size_t CHECKSUM_SIZE = 6;
+static constexpr char SEPARATOR = '1';
 
-    BECH32,  //! Bech32 encoding as defined in BIP173
-    BECH32M, //! Bech32m encoding as defined in BIP350
+enum class Encoding {
+    INVALID, //!< Failed decoding
+
+    BECH32,  //!< Bech32 encoding as defined in BIP173
+    BECH32M, //!< Bech32m encoding as defined in BIP350
+};
+
+/** Character limits for Bech32(m) encoded strings. Character limits are how we provide error location guarantees.
+ *  These values should never exceed 2^31 - 1 (max value for a 32-bit int), since there are places where we may need to
+ *  convert the CharLimit::VALUE to an int. In practice, this should never happen since this CharLimit applies to an address encoding
+ *  and we would never encode an address with such a massive value */
+enum CharLimit : size_t {
+    BECH32 = 90,            //!< BIP173/350 imposed character limit for Bech32(m) encoded addresses. This guarantees finding up to 4 errors.
 };
 
 /** Encode a Bech32 or Bech32m string. If hrp contains uppercase characters, this will cause an
  *  assertion error. Encoding must be one of BECH32 or BECH32M. */
-std::string encode(const std::string& hrp, const std::vector<uint8_t>& values, Encoding encoding);
+std::string Encode(Encoding encoding, const std::string& hrp, const std::vector<uint8_t>& values);
 
-/** A type for the result of decoding. */
 struct DecodeResult
 {
     Encoding encoding;         //!< What encoding was detected in the result; Encoding::INVALID if failed.
@@ -54,8 +54,11 @@ struct DecodeResult
 };
 
 /** Decode a Bech32 or Bech32m string. */
-DecodeResult decode(const std::string& str);
+DecodeResult Decode(const std::string& str, CharLimit limit = CharLimit::BECH32);
 
-}  // namespace bech32
+/** Return the positions of errors in a Bech32 string. */
+std::pair<std::string, std::vector<int>> LocateErrors(const std::string& str, CharLimit limit = CharLimit::BECH32);
 
-#endif  // BECH32_H_
+} // namespace bech32
+
+#endif // BITCOIN_BECH32_H
